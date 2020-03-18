050e82775c9c88f955013f232942d23a5a5f835a
anuprg09 <58516497+anuprg09@users.noreply.github.com>
2020-03-18 16:05:50 +0530

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -148,3 +148,8 @@ STATIC_URL = '/static/'
 # Static file serving.
 # http://whitenoise.evans.io/en/stable/django.html#django-middleware
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
